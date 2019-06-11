45e60accad329c0a52866fb6a9b4ca9cf9eba142
Haroun Khalfi <36559220+Dz1x@users.noreply.github.com>
2019-06-11 15:01:48 -0400

    Deja fini par eux

--- AttaqueMagique.h
+++ AttaqueMagique.h
@@ -2,32 +2,32 @@
 Fichier: AttaqueMagique.h
 Auteur(s): Maude Carrier
 Date de creation: 11 octobre 2016
-Description: Caractèrise l'attaque magique
+Description: CaractÃ¨rise l'attaque magique
 */
 #ifndef ETAT_CREATURE_H
 #define ETAT_CREATURE_H
 
 #include <iostream>
-#include "Creature.h"
+#include "Creature.h" 
 
 class AttaqueMagique
 {
 public:
 	AttaqueMagique(unsigned int duree) { duree_ = duree; }
 
-	virtual ~AttaqueMagique() {} // À MODIFIER (si nécessaire...)
+	virtual ~AttaqueMagique() {} // Ã€ MODIFIER (si nÃ©cessaire...)
 
 	unsigned int obtenirDuree() const { return duree_; }
 	void modifierDuree(unsigned int duree) { duree_ = duree; }
 
 	virtual std::string obtenirTypeAttaque() const { return typeid(AttaqueMagique).name(); }
 
-    virtual void appliquerAttaque(Creature& creature) = 0; //à appliquer sur la créature qui possède ce pouvoir // À MODIFIER (si nécessaire...)
-	virtual bool estFini() const { return true;	} //si vrai, informe la créature qu'elle peut retourner à l'état normal // À MODIFIER (si nécessaire...)
+    virtual void appliquerAttaque(Creature& creature) = 0; //Ã  appliquer sur la crÃ©ature qui possÃ¨de ce pouvoir // Ã€ MODIFIER (si nÃ©cessaire...)
+	virtual bool estFini() const { return true;	} //si vrai, informe la crÃ©ature qu'elle peut retourner Ã  l'Ã©tat normal // Ã€ MODIFIER (si nÃ©cessaire...)
 
-	friend std::ostream& operator<<(std::ostream& o, const AttaqueMagique& attaqueMagique) // À MODIFIER (si nécessaire...)
+	friend std::ostream& operator<<(std::ostream& o, const AttaqueMagique& attaqueMagique) // Ã€ MODIFIER (si nÃ©cessaire...)
 	{ 
-		return o << "Attaque magique de type " << attaqueMagique.obtenirTypeAttaque() << " avec une durée de " << attaqueMagique.duree_ << std::endl;
+		return o << "Attaque magique de type " << attaqueMagique.obtenirTypeAttaque() << " avec une durÃ©e de " << attaqueMagique.duree_ << std::endl;
 	} 
 
 protected:
