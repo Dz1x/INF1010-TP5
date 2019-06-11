841f1068b21ef0ced14f70a06bcf7958a91e30bc
Haroun Khalfi <36559220+Dz1x@users.noreply.github.com>
2019-06-11 02:30:59 -0400

    Terminé

--- PolyLand.cpp
+++ PolyLand.cpp
@@ -21,3 +21,30 @@ bool PolyLand::relacherCreature(Dresseur* dresseur, const std::string& nomCreatu
 	return dresseur->enleverCreature(nomCreature);
 }
 
+ostream & operator<<(ostream & os, const PolyLand & polyland)
+{
+	
+	sort(polyland.listMaitre_.begin(), polyland.listMaitre_.end(), [](Dresseur* dresseur1, Dresseur* dresseur2) {
+		
+
+		return dresseur1->obtenirNom() < dresseur2->obtenirNom();
+		
+	});
+	
+
+	sort(polyland.listCompagnon_.begin(),polyland.listCompagnon_.end(),[](Creature* creature1,Creature* creature2){
+		return creature1->obtenirAttaque() < creature2->obtenirAttaque();
+	});
+
+	for (Dresseur* dresseur : polyland.listMaitre_) {
+
+		os << *dresseur << endl;
+	}
+
+	for (Creature* creature:polyland.listCompagnon_) {
+		os << *creature << endl;
+	}
+
+return os;
+	
+}
